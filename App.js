581ee1de74f8f22267597ad468ed64f220269b70
Mohammad Zaini <Mohammad_zaini@outlook.com>
2023-05-22 21:28:34 +0300

    Setting up Firebase

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components";
-
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-
 import { theme } from "./src/infrastructure/theme"
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
-
 import { Navigation } from "./src/infrastructure/navigation";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB694o8wlclJpUWRdr_weTReML0Bi91I_g",
+  authDomain: "meal-to-go-b785d.firebaseapp.com",
+  projectId: "meal-to-go-b785d",
+  storageBucket: "meal-to-go-b785d.appspot.com",
+  messagingSenderId: "1093092046975",
+  appId: "1:1093092046975:web:a08fea0f8132fb0030674a",
+  measurementId: "G-B11Y9F9N1D"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default App = () => {
   const [oswaldLoaded] = useOswald({
